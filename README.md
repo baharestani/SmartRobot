75599da0ab4c51f294e1d46deb816bf7bd11d80f
Daniel Baharestani <baharestani@gmail.com>
2017-12-26 17:16:39 +1100

    Moved source code into src folder. Added readme.md

---
+++ README.md
@@ -0,0 +1,3 @@
+Smart Robot is a square that can follow your mouse cursor. It is smart enough to get around the obstacles in its way and choose the shortest path.
+
+![](image/screenshot.png)
--- src/SmartRobot.Console/Program.cs
+++ src/SmartRobot.Console/Program.cs
@@ -1,11 +1,4 @@
-﻿using SmartRobot;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SmartRobot.Console
+﻿namespace SmartRobot.Console
 {
     class Program
     {
